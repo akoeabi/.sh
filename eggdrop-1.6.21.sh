b64d1ae1cb402c7f7e0398d1704bebf104301a97
Hasbullah Masby <63797095+akoeabi@users.noreply.github.com>
2020-04-16 23:46:56 +0800

    Create eggdrop-1.6.21.sh

---
+++ eggdrop-1.6.21.sh
@@ -0,0 +1,27 @@
+echo ""
+echo "AssaLamuaLaikum WarahmatuLLah"
+echo "Selamat Datang di UkhuwahNet Teams automated Enggdrop builder!"
+echo ""
+echo "Script ini akan membantu anda melakukan instalasi Enggdrop dengan cepat"
+echo "Proses instalasi akan memakan waktu beberapa saat. Jadi mohon di tunggu sampai prosesnya selesai"
+echo "ada pertanyaan, silahkan menghubungi staff kami"
+echo ""
+echo "Tekan ENTER untuk melanjutkan"; read devnull
+echo ""
+echo "Proses instalasi sedang berlangsung - mohon menunggu..."
+echo ""
+echo "Downloading eggdrop version 1.6.21..."
+mkdir eggdrop
+cd eggdrop
+wget http://164.132.231.84/hasby/downloads/eggdrop/eggdrop-1.6.21.tgz
+tar -xzvf eggdrop-1.6.21.tgz
+ls -al
+rm -rf eggdrop-1.6.21.tgz
+echo ""
+echo "Terima kasih karena mempercayakan Teams Hosting anda kepada Ukhuwahnet Teams"
+echo "Eggdrop berhasil terinstal"
+echo "Configurasi dari eggdrop (eggdrop.conf) sudah kami hapus agar anda bisa memasang configurasi bot anda sendiri. Terima kasih"
+echo ""
+echo "WassaLamuaLaikum WarahmatuLLah"
+echo "Team Support Ukhuwahnet Teams"
+echo ""
